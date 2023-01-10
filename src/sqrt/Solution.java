4d47f9e8cd3fe103bbd02f5cfbab280bf536f057
Marlen Temirbaev <mouflon150@gmail.com>
2023-01-10 17:15:10 +0600

    Commit message

--- src/sqrt/Solution.java
+++ src/sqrt/Solution.java
@@ -2,11 +2,16 @@ package sqrt;
 
 public class Solution {
 
-    public int mySqrt(int x) {
+    public static int unaryOperator(int x) {
 
         if (x > 0) {
             return (int) Math.sqrt(x);
         }
         return 0;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(unaryOperator(4));
     }
 }
